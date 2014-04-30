a85bcd8853a1b7de0c532ed0db0697902dc62320
Tom Marble <tmarble@info9.net>
2014-04-30 16:19:13 -0500

    make servlet-api a full dependency

--- project.clj
+++ project.clj
@@ -7,15 +7,11 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
-                 [environ "0.5.0"]]
-                 ;; [ring/ring-jetty-adapter "1.1.0"]
-                 ;; [ring/ring-devel "1.1.0"]
-                 ;; [ring-basic-authentication "1.0.1"]
-                 ;; [com.cemerick/drawbridge "0.0.6"]]
+                 [environ "0.5.0"]
+                 [javax.servlet/servlet-api "2.5"]]
   :plugins [[lein-ring "0.8.10"]
             [lein-environ "0.5.0"]]
   :ring {:handler chat-nickname.web/app}
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}
+  {:dev {:dependencies [[ring-mock "0.1.5"]]}
    :production {:env {:production true}}})
